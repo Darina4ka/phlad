3d71180051c17eec279663d09304f107d653a644
Daria <dashamelnik293@gmail.com>
2022-09-02 21:37:44 +0300

    third task

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿try
+{
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32 (Console.ReadLine ());
+if (a % 2 == 0)
+{
+Console.WriteLine ("Число чётное");
+}
+else Console.WriteLine ("Число нечётное");
+}
+catch
+{
+Console.WriteLine ("error");
+}
