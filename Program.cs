8fc81a89949753807ea579dae80a2307acf6e464
Daria <dashamelnik293@gmail.com>
2022-09-02 21:52:23 +0300

    fouth task

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿try
+{
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32 (Console.ReadLine ());
+int i = 1;
+while (i <= N)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine (i);
+        i = i + 2;
+    }
+    else i++;
+}
+}
+catch
+{
+Console.WriteLine ("error");
+}
